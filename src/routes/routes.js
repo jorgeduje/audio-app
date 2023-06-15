080ef76911427b701fa32df6bdb51058df6fa420
jorgeduje <jorgeduje4@gmail.com>
2023-06-15 16:36:40 -0300

    scalfolding para la app

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import HomeContainer from "../components/pages/home/HomeContainer";
+
+
+export const menuRoutes = [
+    {
+        id:"home",
+        path: "/",
+        Element: HomeContainer
+    },
+]
