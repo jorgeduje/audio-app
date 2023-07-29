291817cf7f5773e29969ef9719a279fc59f0690f
jorgeduje <jorgeduje4@gmail.com>
2023-07-29 19:43:26 -0300

    push

--- src/components/pages/pagoExitoso/PagoExitoso.jsx
+++ src/components/pages/pagoExitoso/PagoExitoso.jsx
@@ -1,6 +1,12 @@
+import axios from "axios"
+import { useEffect } from "react"
 
 const PagoExitoso = () => {
     console.log("se compro correctamente")
+    useEffect(()=>{
+      const data = axios.post("https://prueba-mp.vercel.app/webhook")
+      data.then(res => console.log("res de webhook", res))
+    },[])
   return (
     <div>PagoExitoso</div>
   )
