bdca5b4a92281f900d74812e61bbb98d92145f3d
jorgeduje <jorgeduje4@gmail.com>
2023-07-31 10:17:59 -0300

    adapta logica aprovved

--- src/components/pages/checkout/CheckoutContainer.jsx
+++ src/components/pages/checkout/CheckoutContainer.jsx
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSelector } from "react-redux";
+import { db } from "../../../firebaseConfig";
 
 const CheckoutContainer = () => {
+  const { cart } = useSelector((store) => store.cartSlice);
   const [preferenceId, setPreferenceId] = useState(null);
-  initMercadoPago(import.meta.env.VITE_PUBLICKEY);
+  initMercadoPago(import.meta.env.VITE_PUBLICKEY, { locale: "es-AR" });
+  // LUEGO DE PAGAR
+  const [orderId, setOrderId] = useState("");
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const paramValue = queryParams.get("status");
+  const [pagando, setPagando] = useState(false);
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    if (paramValue === "aprovved") {
+      let order = {
+        name: userData.name,
+        email: userData.email,
+        items: cart,
+        date: serverTimestamp(),
+      };
+      let ordersCollections = collection(db, "orders");
+      addDoc(ordersCollections, order).then((res) => setOrderId(res.id));
+    }
+  }, [paramValue]);
 
   const createPreference = async () => {
     try {
       const response = await axios.post(
         "https://prueba-mp.vercel.app/create_preference",
         [
-          {
-            title: "zapatilla nilo",
-            unit_price: 15,
-            quantity: 3,
-          },
-          {
-            title: "zapatilla nilo",
-            unit_price: 20,
-            quantity: 2,
-          },
+          { title: "x", unit_price: 5, quantity: 2 },
+          { title: "y", unit_price: 4, quantity: 3 },
         ]
       );
 
@@ -37,11 +57,37 @@ const CheckoutContainer = () => {
       setPreferenceId(id);
     }
   };
-
+  const funcionDeLosInput = (evento) => {
+    setUserData({ ...userData, [evento.target.name]: evento.target.value });
+  };
   return (
     <div>
-      <button onClick={handleBuy}>Comprar</button>
-      {preferenceId && <Wallet initialization={{ preferenceId }} />}
+      {!pagando && (
+        <div>
+          <input
+            type="text"
+            placeholder="ingrese su nombre"
+            name="name"
+            onChange={funcionDeLosInput}
+          />
+          <input
+            type="text"
+            placeholder="ingrese su email"
+            name="email"
+            onChange={funcionDeLosInput}
+          />
+        </div>
+      )}
+      <button onClick={handleBuy}>Seleccione metodo de pago</button>
+      {preferenceId && (
+        <Wallet
+          initialization={{ preferenceId }}
+          onReady={() => setPagando(true)}
+        />
+      )}
+      {orderId && (
+        <h2>Su compra fue exitosa el numero de comprobante es {orderId}</h2>
+      )}
     </div>
   );
 };
--- src/components/pages/home/HomeContainer.jsx
+++ src/components/pages/home/HomeContainer.jsx
@@ -1,28 +1,9 @@
-import { useEffect, useState } from "react";
 import Home from "./Home";
-import axios from "axios";
-import { useLocation } from "react-router-dom";
 
 const HomeContainer = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  console.log({ queryParams });
-  const paramValue = queryParams.get("status");
-
-  const [data, setData] = useState(null);
-  const [param, setParam] = useState(null);
-
-  useEffect(() => {
-    setParam(paramValue);
-    const data = axios.post(
-      `https://prueba-mp.vercel.app/webhook/${location.search}`
-    );
-    data.then((res) => setData(res.data));
-  }, [location.search, paramValue]);
+ 
   return (
     <div>
-      {data && JSON.stringify(data)}
-      {param && JSON.stringify(param)}
 
       <Home />
     </div>
